5eff70fe3aa801b72d7fb033d6d96901017167a9
Shruti-jha6 <shruti119btcsai22@igdtuw.ac.in>
2024-04-16 04:31:35 +0530

    levelwise

--- src/Components/Explore.js
+++ src/Components/Explore.js
@@ -1,41 +1,41 @@
-import React from 'react'
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import './Explore.css';
-import map from './Images/mapquality.png'
+import map from './Images/mapquality.png';
 import Health from './HealthForm';
 import Result from './Result';
 import './Result.css';
+
 function Explore() {
+  const navigate = useNavigate(); // Move it here
+
   return (
     <>
-    
-    <div className='explore1'>
-      <h1>Let's Explore</h1>
-    </div>
-    <div className="explore">
-      
-      <iframe 
-      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d224150.4205162784!2d76.92706373233423!3d28.61926090955083!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390d1aafdb891567%3A0x10d270731c930a87!2sGuru%20Gobind%20Singh%20Indraprastha%20University!5e0!3m2!1sen!2sin!4v1713197862067!5m2!1sen!2sin" 
-      width="600" 
-      height="450" 
-      style={{border:"0"}}
-      allowfullscreen="" 
-      loading="lazy" 
-      referrerpolicy="no-referrer-when-downgrade">
-
-      </iframe>
-      
-      
-     
-    </div>,
-    <Health/>,
-    <Result/>
-    <div className='map'>
-    <img src={map} alt="Card" className="card-image" />
-
-    </div>
+      <div className='explore1'>
+        <h1>Let's Explore</h1>
+      </div>
+      <div className="explore">
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d224150.4205162784!2d76.92706373233423!3d28.61926090955083!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390d1aafdb891567%3A0x10d270731c930a87!2sGuru%20Gobind%20Singh%20Indraprastha%20University!5e0!3m2!1sen!2sin!4v1713197862067!5m2!1sen!2sin"
+          width="600"
+          height="450"
+          style={{ border: "0" }}
+          allowfullscreen=""
+          loading="lazy"
+          referrerpolicy="no-referrer-when-downgrade">
+        </iframe>
+      </div>
+      <Health />
+      <Result />
+      <div className='but-place'>
+        <button className='but2' onClick={() => navigate("/about")}>Checkout Medicines</button>
+        <button className='but2' onClick={() => navigate("/about")}>Checkout Products</button>
+      </div>
+      <div className='map'>
+        <img src={map} alt="Card" className="card-image" />
+      </div>
     </>
-    
-  )
+  );
 }
 
-export default Explore
+export default Explore;
