e6aaed9fa9b3a741a1ecf14847f919440e95c206
Shruti-jha6 <shruti119btcsai22@igdtuw.ac.in>
2024-04-16 07:46:59 +0530

    profile

--- src/Components/Profile.js
+++ src/Components/Profile.js
@@ -1,11 +1,14 @@
 import React, { useState } from 'react';
-import './Profile.css'; // Import CSS file for styling
+import './Profile.css'; 
+import profilepic from './Images/pic2.png';
 
 function Profile() {
   const userDetails = {
-    username: "JohnDoe",
-    email: "john@example.com",
+    username: "Shubh krishna",
+    email: "shubhkrishna2004@gmail.com",
     points: 500,
+    age:20,
+    disease: "Sinus"
   };
 
   const [isLoggedIn, setIsLoggedIn] = useState(true);
@@ -19,17 +22,26 @@ function Profile() {
   };
 
   return (
+    <>
+    
     <div className="profile">
       {isLoggedIn ? (
+        <>
+        <div className='profile-pic'>
+        <img src={profilepic} alt=" profile picture"/>
+      </div>
         <div className="profile-details">
-          <h1>Welcome, {userDetails.username}!</h1>
-          <p>Email: {userDetails.email}</p>
-          <p>Points: {userDetails.points}</p>
+          <h1 className='welcome'>Welcome, {userDetails.username}!</h1>
+          <p className='email'>Email: {userDetails.email}</p>
+          <p className='age'>Age:{userDetails.age}</p>
+          <p className='disease'>Disease:{userDetails.disease}</p>
+
           <div className="buttons">
             <button className="logout-button" onClick={handleLogout}>Logout</button>
-            <button className="new-id-button" onClick={handleNewId}>Create New ID</button>
+            <button className="new-id-button" onClick={handleNewId}>Edit Profile</button>
           </div>
         </div>
+        </>
       ) : (
         <div className="login">
           <h1>Profile</h1>
@@ -37,7 +49,9 @@ function Profile() {
           <button className="login-button" onClick={() => setIsLoggedIn(true)}>Login</button>
         </div>
       )}
+      
     </div>
+    </>
   );
 }
 
